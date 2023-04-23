e33419cb55c25f10cae0c7870db314390ffbc63c
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-04-23 20:28:52 +0300

    #3 add button

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [number1, setNumber1] = useState(0)
+  const addToNumber = () => {
+    setNumber1(number1 + 1)
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.txt}>Hello World!</Text>
+      <Text style={styles.txt}>The Number is: {number1}</Text>
+      <TouchableOpacity style={styles.button1} onPress={() => addToNumber()}>
+      <Text style={styles.txt2}>Add</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,6 +27,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   txt:{
-    fontSize:50,
-  }
+    fontSize:25,
+  },
+  txt2:{
+    fontSize:45,
+    color: 'white',
+  },
+  button1:{
+    backgroundColor: 'blue',
+    fontSize:45,
+    padding: 5,
+    borderRadius: 10,
+    margin: 10,
+  },
 });
