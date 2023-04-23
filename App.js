091b0b44c3ec8e27a8434336bd1572a569f2aeab
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-04-23 17:57:09 +0300

    #2

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.txt}>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  txt:{
+    fontSize:50,
+  }
 });
