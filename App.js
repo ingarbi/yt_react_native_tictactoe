f4364213b43350fc0e830282e58b8a096e3bb6c6
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-04-24 18:58:06 +0300

    #6 winnig conditio

--- App.js
+++ App.js
@@ -17,27 +17,61 @@ export default function App() {
     " "," "," ",
   ]);
 
-  const [ currentPlayer, setCurrentPlayer] = useState("X")
+  const [currentPlayer, setCurrentPlayer] = useState("X");
 
-  const pressField = (index) =>{
-    let newBoard = board
+  const pressField = (index) => {
+    let newBoard = board;
 
-  if(newBoard[index] !== "X" && newBoard[index] !== "O"){
-    if(currentPlayer == "X"){
-      newBoard[index] = "X"
-      setCurrentPlayer("O")
-      setNotification("Player O turn")
-    }else{
-      newBoard[index] = "O"
-      setCurrentPlayer("X")
-      setNotification("Player X turn")
+    if (newBoard[index] !== "X" && newBoard[index] !== "O") {
+      if (currentPlayer == "X") {
+        newBoard[index] = "X";
+        setCurrentPlayer("O");
+        setNotification("Player O turn");
+      } else {
+        newBoard[index] = "O";
+        setCurrentPlayer("X");
+        setNotification("Player X turn");
+      }
+
+      setBoard(newBoard);
+      setRefresh(!refresh);
+      checkIfPlayerWin();
     }
+  };
 
-    setBoard(newBoard)
-    setRefresh(!refresh)
+  const checkIfPlayerWin = () =>{
+    if (board[0] === board[1] && board[1] === board[2] && board[0] !== ' '){
+      playerWon(board[0])
+    }else if(board[3] === board[4] && board[4] === board[5] && board[5] !== ' '){
+      playerWon(board[3])
+    }else if(board[6] === board[7] && board[7] === board[8] && board[8] !== ' '){
+      playerWon(board[6])
+    }else if(board[0] === board[4] && board[4] === board[8] && board[8] !== ' '){
+      playerWon(board[0])
+    }else if(board[0] === board[3] && board[3] === board[6] && board[6] !== ' '){
+      playerWon(board[0])
+    }else if(board[1] === board[4] && board[4] === board[7] && board[7] !== ' '){
+      playerWon(board[1])
+    }else if(board[2] === board[4] && board[4] === board[6] && board[6] !== ' '){
+      playerWon(board[2])
+    }else if(board[2] === board[5] && board[5] === board[8] && board[8] !== ' '){
+      playerWon(board[2])
+    }
+  };
+  const playerWon = (symbol) =>{
+    alert("Player " + symbol + " won")
+    setBoard([
+      " "," "," ",
+      " "," "," ",
+      " "," "," ",
+    ])
+    if(symbol =="O"){
+      setNotification("X to start")
+    }else{
+      setNotification("O to start")
+    }
   }
-    
-  }
+
 
   return (
     <View style={styles.container}>
@@ -49,8 +83,11 @@ export default function App() {
         data={board}
         numColumns={3}
         extraData={refresh}
-        renderItem={({item, index}) => (
-          <TouchableOpacity style={styles.square} onPress={() => pressField(index)}>
+        renderItem={({ item, index }) => (
+          <TouchableOpacity
+            style={styles.square}
+            onPress={() => pressField(index)}
+          >
             <Text style={styles.txt2}>{item}</Text>
           </TouchableOpacity>
         )}
@@ -84,9 +121,9 @@ const styles = StyleSheet.create({
     height: 400,
   },
   square: {
-    height:60,
+    height: 60,
     width: 40,
     backgroundColor: "blue",
     margin: 10,
-  }
+  },
 });
